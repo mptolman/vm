76cf4aca2b6c2a4593adced9ef2d43258deb9a66
Matthew Tolman <matt@mptolman.net>
2014-03-13 15:48:35 -0600

    lexer

--- lexer.d
+++ lexer.d
@@ -1,15 +1,13 @@
 import std.ascii;
 import std.conv;
-import std.stdio;
 import std.stream;
-import std.string;
 import queue;
 
 enum TType : byte
 {
     LABEL,
-    OPCODE,
     REGISTER,
+    OPCODE,
     OPAREN,
     CPAREN,
     COMMA,
@@ -34,29 +32,26 @@ class Lexer
 public:
     this(string fileName)
     {
-        _file   = new BufferedFile(fileName);
+        _file   = new BufferedFile(fileName, FileMode.In);
         _tokens = new Queue!Token;
     }
 
-    Token peek()
+    auto peek()
     {
-        if (!_tokens.empty)
-            return _tokens.front;
+        if (_tokens.empty)
+            loadMoreTokens();
 
-        loadMoreTokens();
-        return peek();
+        return _tokens.front;
     }
 
-    Token next()
+    auto next()
     {
-        if (!_tokens.empty) {
-            auto t = _tokens.front;
-            _tokens.pop();
-            return t;
-        }
+        if (_tokens.empty)
+            loadMoreTokens();
 
-        loadMoreTokens();
-        return next();
+        auto t = _tokens.front;
+        _tokens.pop();
+        return t;
     }
 
     void rewind()
@@ -76,105 +71,98 @@ private:
     size_t _lineNum;
 
     static immutable TType[string] tokMap;
+    static immutable size_t BUFFER_SIZE = 500;
 
     void loadMoreTokens()
     {
         static char[] buffer;
 
-        if (_file.eof) {
-            _tokens.push(Token(TType.EOF, null, _lineNum));
-            return;
-        }
+        while(!_file.eof) {
+            _line = to!string(_file.readLine(buffer));
+            _line ~= '\n';
+            ++_lineNum;
 
-        _line = to!string(_file.readLine(buffer));
-        _line ~= '\n';
-        ++_lineNum;
+            for (_pos = 0; _pos < _line.length;) {
+                char c  = _line[_pos++];
+                _lexeme = [c];
 
-        for (_pos = 0; _pos < _line.length;) {
-            char c  = _line[_pos];
-            _lexeme = [c];
+                if (isWhite(c)) {
+                    // ignore whitespace
+                }                
+                else if (isAlpha(c))
+                    alpha();
 
-            if (isWhite(c)) {
-                ++_pos;
+                else if (isDigit(c))
+                    int_literal();
+
+                else if (c == '\'')
+                    byt_literal();
+
+                else if (c == '.')
+                    directive();
+
+                else if (c == '-')
+                    minus();
+
+                else if (c == ',')
+                    _tokens.push(Token(TType.COMMA, _lexeme, _lineNum));
+
+                else if (c == '(')
+                    _tokens.push(Token(TType.OPAREN, _lexeme, _lineNum));
+
+                else if (c == ')')
+                    _tokens.push(Token(TType.CPAREN, _lexeme, _lineNum));
+
+                else if (c == ';')
+                    break;
+
+                else
+                    _tokens.push(Token(TType.UNKNOWN, _lexeme, _lineNum));
             }
-            else if (isAlpha(c)) {
-                alpha();
-            }
-            else if (c == '.') {
-                directive();
-            }
-            else if (isDigit(c)) {
-                int_literal();
-            }
-            else if (c == '\'') {
-                byt_literal();
-            }
-            else if (c == '-') {
-                minus();
-            }
-            else if (c == ',') {
-                ++_pos;
-                _tokens.push(Token(TType.COMMA, _lexeme, _lineNum));
-            }
-            else if (c == '(') {
-                ++_pos;
-                _tokens.push(Token(TType.OPAREN, _lexeme, _lineNum));
-            }
-            else if (c == ')') {
-                ++_pos;
-                _tokens.push(Token(TType.CPAREN, _lexeme, _lineNum));
-            }
-            else if (c == ';') {
+
+            if (_tokens.size >= BUFFER_SIZE)
                 break;
-            }
-            else {
-                ++_pos;
-                _tokens.push(Token(TType.UNKNOWN, _lexeme, _lineNum));
-            }
         }
+
+        if (_file.eof)
+            _tokens.push(Token(TType.EOF, null, _lineNum));        
     }
 
     auto collectWhile(bool function(char c) f)
     {
         string tok;
-        while (_pos < _line.length) {
-            if (f(_line[_pos]))
-                tok ~= _line[_pos++];
-            else
+
+        foreach (c; _line[_pos..$]) {
+            if (f(c))
+                tok ~= c;
+            else 
                 break;
         }
+
+        _pos += tok.length;
         return tok;
     }
 
     void alpha()
     {
-        ++_pos;
         _lexeme ~= collectWhile(c => isAlphaNum(c) || c == '_');
         _tokens.push(Token(_lexeme in tokMap ? tokMap[_lexeme] : TType.LABEL, _lexeme, _lineNum));
     }
 
     void directive()
     {
-        ++_pos;
         _lexeme ~= collectWhile(c => isAlpha(c));
-        if (_lexeme == ".BYT")
-            _tokens.push(Token(TType.BYT_DIRECTIVE, _lexeme, _lineNum));
-        else if (_lexeme == ".INT")
-            _tokens.push(Token(TType.INT_DIRECTIVE, _lexeme, _lineNum));
-        else
-            _tokens.push(Token(TType.UNKNOWN, _lexeme, _lineNum));
+        _tokens.push(Token(_lexeme in tokMap ? tokMap[_lexeme] : TType.UNKNOWN, _lexeme, _lineNum));   
     }
 
     void int_literal()
     {
-        ++_pos;
         _lexeme ~= collectWhile(c => isDigit(c));
         _tokens.push(Token(TType.INT_LITERAL, _lexeme, _lineNum));
     }
 
     void byt_literal()
     {
-        ++_pos;
         _tokens.push(Token(TType.BYT_DELIM, _lexeme, _lineNum));
         _lexeme = collectWhile(c => c != '\'');
         if (_lexeme.length)
@@ -185,35 +173,69 @@ private:
 
     void minus()
     {
-        if (isDigit(_line[_pos+1])) {
+        if (isDigit(_line[_pos]))
             int_literal();
-        }
-        else {
-            ++_pos;
+        else
             _tokens.push(Token(TType.UNKNOWN, _lexeme, _lineNum));
-        }
     }
 
     static this()
     {
         tokMap = [
-            "R0"  : TType.REGISTER,
-            "R1"  : TType.REGISTER,
-            "R2"  : TType.REGISTER,
-            "R3"  : TType.REGISTER,
-            "R4"  : TType.REGISTER
+            ".INT" : TType.INT_DIRECTIVE,
+            ".BYT" : TType.BYT_DIRECTIVE,
+            "R0"   : TType.REGISTER,
+            "R1"   : TType.REGISTER,
+            "R2"   : TType.REGISTER,
+            "R3"   : TType.REGISTER,
+            "R4"   : TType.REGISTER,
+            "R5"   : TType.REGISTER,
+            "R6"   : TType.REGISTER,
+            "R7"   : TType.REGISTER,
+            "R8"   : TType.REGISTER,
+            "R9"   : TType.REGISTER,
+            "PC"   : TType.REGISTER,
+            "SP"   : TType.REGISTER,
+            "SB"   : TType.REGISTER,
+            "SL"   : TType.REGISTER,
+            "FP"   : TType.REGISTER,
+            "HP"   : TType.REGISTER,
+            "ADD"  : TType.OPCODE,
+            "ADI"  : TType.OPCODE,
+            "AND"  : TType.OPCODE,
+            "BGT"  : TType.OPCODE,
+            "BLK"  : TType.OPCODE,
+            "BNZ"  : TType.OPCODE,
+            "BRZ"  : TType.OPCODE,
+            "CMP"  : TType.OPCODE,
+            "DIV"  : TType.OPCODE,
+            "END"  : TType.OPCODE,
+            "JMP"  : TType.OPCODE,
+            "JMR"  : TType.OPCODE,
+            "LCK"  : TType.OPCODE,
+            "LDA"  : TType.OPCODE,
+            "LDB"  : TType.OPCODE,
+            "LDR"  : TType.OPCODE,
+            "MOV"  : TType.OPCODE,
+            "MUL"  : TType.OPCODE,
+            "OR"   : TType.OPCODE,
+            "RUN"  : TType.OPCODE,
+            "STB"  : TType.OPCODE,
+            "STR"  : TType.OPCODE,
+            "SUB"  : TType.OPCODE,            
+            "TRP"  : TType.OPCODE,
+            "ULK"  : TType.OPCODE
         ];
     }
 }
 
-void main()
-{
-    auto tokens = std.stdio.File(r"C:\tokens.txt", "w");
-
-    Lexer lex = new Lexer(r"asm\proj2.asm");
-    Token t;
-    do {
-        t = lex.next();
-        tokens.writeln(t);
-    } while (t.type != TType.EOF);
-}
+//void main()
+//{
+//    auto file = std.stdio.File(r"C:\tokens.txt", "w");
+//    Lexer lex = new Lexer(r"asm\proj3.asm");
+//    Token t = lex.next();
+//    do {
+//        file.writeln(t);
+//        t = lex.next();
+//    } while (t.type != TType.EOF);
+//}
